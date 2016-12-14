c058d9e742d023d9ce07f5dfa1a579d17ce7539c
Harrison <Harrison>
2016-12-13 22:05:16 -0500

    added spinning enemy

---
+++ script/SpinningFixedPlatform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningFixedPlatform : MonoBehaviour {
+
+	public SpinningScript parent;
+
+	void Update () {
+		transform.Rotate (0, 0, -parent.rotationSpeed);
+	}
+}
