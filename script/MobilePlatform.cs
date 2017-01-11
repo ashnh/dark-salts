a5c3263220b15e90355b90da0b5cef7945049fef
Harrison <Harrison>
2017-01-11 00:10:45 -0500

    added dest wompum and worked on trapezoid block

--- script/MobilePlatform.cs
+++ script/MobilePlatform.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class MobilePlatform : MonoBehaviour {
 
+	public float localPositionOfX;
+	public float localPositionOfY;
+
+	public bool fixPosition;
+
 	public Collider2D player;
 
 	public GameObject Parent;
 
 	// Update is called once per frame
 	void Update () {
+
+		if (fixPosition) {
+			transform.localPosition = new Vector3 (localPositionOfX, localPositionOfY, 0);
+		}
 
 		GetComponent <Rigidbody2D> ().velocity = Parent.GetComponent <Rigidbody2D> ().velocity;
 
