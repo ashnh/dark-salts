f939950d143449941f0ff3930bc17629b4c8fb1b
Harrison <Harrison>
2017-02-05 22:55:09 -0500

    square block touched up and floor 3 sprites added to project

---
+++ script/FloatingPlatformHelp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingPlatformHelp : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		if (other.gameObject.GetComponentInParent <Player> () != null && other.tag == "jumpBox") {
+			other.gameObject.GetComponentInParent <Rigidbody2D> ().velocity = new Vector3 (0, 0, 0);
+		}
+
+	}
+
+}
--- script/SpinningFixedPlatform.cs
+++ script/SpinningFixedPlatform.cs
@@ -3,9 +3,44 @@ using System.Collections;
 
 public class SpinningFixedPlatform : MonoBehaviour {
 
+	public float distFromPlayer;
+
+	public GameObject player;
+
+	public Collider2D jumpBox;
+
 	public SpinningScript parent;
+
+	bool inTrigger;
+
+	public SpinningFixedPlatform () {
+		inTrigger = false;
+	}
 
 	void Update () {
 		transform.Rotate (0, 0, -parent.rotationSpeed);
+
+		if (inTrigger && !(Input.GetKey (KeyCode.W))) {
+
+			player.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y + distFromPlayer, 0);
+
+			player.GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
+
+		}
+
 	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "jumpBox") {
+			inTrigger = true;
+		}
+
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.tag == "jumpBox") {
+			inTrigger = false;
+		}
+	}
+
 }
