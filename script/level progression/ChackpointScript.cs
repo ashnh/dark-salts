d48f5a99b710f861aa067d89b410c61ccd06473e
Harrison <Harrison>
2017-04-01 22:33:42 -0400

    major polishing

--- script/level progression/ChackpointScript.cs
+++ script/level progression/ChackpointScript.cs
@@ -3,20 +3,30 @@ using System.Collections;
 
 public class ChackpointScript : MonoBehaviour {
 
+	public Sprite off;
+	public Sprite on;
+
+	bool isOn;
+
 	// Use this for initialization
 	void Start () {
-	
+		isOn = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isOn) {
+			GetComponent <SpriteRenderer> ().sprite = on;
+		} else {
+			GetComponent <SpriteRenderer> ().sprite = off;
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Player") {
 			PlayerPrefs.SetFloat ("checkY", transform.position.y);
 			PlayerPrefs.SetFloat ("checkX", transform.position.x);
+			isOn = true;
 		}
 	}
 
--- script/level progression/HubTwoScript.cs
+++ script/level progression/HubTwoScript.cs
@@ -6,21 +6,87 @@ public class HubTwoScript : MonoBehaviour {
 	public GameObject map;
 	public GameObject gate;
 
+	public Sprite oneTwoThree;
+	public Sprite oneTwo;
+	public Sprite oneThree;
+	public Sprite TwoThree;
+	public Sprite one;
+	public Sprite two;
+	public Sprite three;
+	public Sprite zero;
+
 	public int levelOneNumber;
 	public int levelTwoNumber;
 	public int levelThreeNumber;
 
-	int lOne;
-	int lTwo;
-	int lThree;
+	bool lOneDone;
+	bool lTwoDone;
+	bool lThreeDone;
 
 	// Use this for initialization
 	void Start () {
-		
+		lOneDone = 0 < PlayerPrefs.GetInt ("level " + levelOneNumber + " loaded");
+		lTwoDone = 0 < PlayerPrefs.GetInt ("level " + levelTwoNumber + " loaded");
+		lThreeDone = 0 < PlayerPrefs.GetInt ("level " + levelThreeNumber + " loaded");
+		if (lOneDone) {
+			if (lTwoDone) {
+				if (lThreeDone) {
+					//load 1,2,3 & open gate
+					map.GetComponent <SpriteRenderer> ().sprite = oneTwoThree;
+					Destroy (gate);
+				} else {
+					//load 1,2
+					map.GetComponent <SpriteRenderer> ().sprite = oneTwo;
+				}
+			} else {
+				if (lThreeDone) {
+					//load 1,3
+					map.GetComponent <SpriteRenderer> ().sprite = oneThree;
+				} else {
+					//load 1
+					map.GetComponent <SpriteRenderer> ().sprite = one;
+				}
+			}
+		} else {
+			if (lTwoDone) {
+				if (lThreeDone) {
+					//load 2,3
+					map.GetComponent <SpriteRenderer> ().sprite = TwoThree;
+				} else {
+					//load 2
+					map.GetComponent <SpriteRenderer> ().sprite = two;
+				}
+			} else {
+				if (lThreeDone) {
+					//load 3
+					map.GetComponent <SpriteRenderer> ().sprite = three;
+				} else {
+					//load 0
+					map.GetComponent <SpriteRenderer> ().sprite = zero;
+				}
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			PlayerPrefs.SetInt ("level " + levelOneNumber + " loaded", 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			PlayerPrefs.SetInt ("level " + levelTwoNumber + " loaded", 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			PlayerPrefs.SetInt ("level " + levelThreeNumber + " loaded", 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			PlayerPrefs.SetInt ("level " + levelOneNumber + " loaded", 1);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			PlayerPrefs.SetInt ("level " + levelTwoNumber + " loaded", 1);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha6)) {
+			PlayerPrefs.SetInt ("level " + levelThreeNumber + " loaded", 1);
+		}
 	}
 }
